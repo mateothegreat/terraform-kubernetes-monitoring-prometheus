f283801d074f07d6a483249168cd8de80a654ceb
Matthew Davis <matthew@matthewdavis.io>
2021-05-04 11:35:34 -0400

    bump

--- test/main.tf
+++ test/main.tf
@@ -1,3 +1,32 @@
+terraform {
+
+    required_providers {
+
+        kubernetes-alpha = {
+
+            source  = "hashicorp/kubernetes-alpha"
+            version = "0.2.1"
+
+        }
+
+        kubernetes = {
+
+            source  = "hashicorp/kubernetes"
+            version = "2.0.0"
+
+        }
+
+        aws = {
+
+            source  = "hashicorp/aws"
+            version = "3.30.0"
+
+        }
+
+    }
+
+}
+
 variable "cluster_name" {}
 
 #
